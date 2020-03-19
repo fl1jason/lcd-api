1f2c125619f0f45c67e7180930b110d3e802f119
fl1jason <jason@fl1digital.com>
2020-03-19 22:18:29 +0000

    fix to Month in Timestamp

--- app/util/datetime.functions.js
+++ app/util/datetime.functions.js
@@ -4,7 +4,7 @@ exports.getTimestamp = getTimestamp
 function getTimestamp() 
 {
     const today = new Date();
-    const date = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
+    const date = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`;
     const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
     const timeStamp = `${date}T${time}`;
 
